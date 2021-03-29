09f4475e47bd6a0225c047ab0c9c9f75a467e80a
REHAN LC <frayhands@gmail.com>
2021-03-29 21:14:12 +0700

    [IMP] finish create room

--- internal/models/game_room.go
+++ internal/models/game_room.go
@@ -2,8 +2,9 @@ package models
 
 type GameRoom struct {
 	ID                 string `json:"id"`
+	RoomTitle          string `json:"title"`
 	RoomMasterUsername string `json:"room_master_username"`
 	Mode               string `json:"mode"`
-	CurrentPlayerCount string `json:"current_player_count"`
-	MaxPlayerCount     string `json:"max_player_count"`
+	CurrentPlayerCount int    `json:"current_player_count"`
+	MaxPlayerCount     int    `json:"max_player_count"`
 }
